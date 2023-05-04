0dac3dd25eec1638d99e8a635cadb47d138cdc14
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-05-04 06:39:50 +0530

    Update Next Config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        runtime: 'edge'
-    }
-}
+const nextConfig = {};
+
 
 module.exports = nextConfig
