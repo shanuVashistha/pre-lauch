b2ffd3593ce12112dfe8514ff7ee3eb2a2f32942
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-05-04 12:42:33 +0530

    Update Next Config With Formats

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     experimental: {
         runtime: 'edge'
     },
-    formats: ['image/webp', 'image/png', 'image/svg']
+    formats: ['image/webp', 'image/png', 'image/svg', 'image/jpg', 'image/jpeg']
 }
 
 module.exports = nextConfig
