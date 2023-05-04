097c065cb3449b7606cc5d60695c0d79233f65e1
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-05-04 06:24:39 +0530

    Bug Fixed

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-const prod = process.env.NODE_ENV === 'production'
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true'
-})
-module.exports = withBundleAnalyzer(withPWA({
-    pwa: {
-        disable: prod ? false : true,
-        dest: 'public',
-        runtimeCaching: prod ? runtimeCaching : undefined,
-    },
-}))
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        runtime: 'edge'
+    }
+}
+
+module.exports = nextConfig
